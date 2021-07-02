0900003515ee412f203d1e7047404c66c8189a73
ryuzaki <ryuzaki>
2021-07-02 11:48:12 +0530

    integration

--- app/lib/pages/register.dart
+++ app/lib/pages/register.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:chat_app/pages/chat.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Register extends StatefulWidget {
   static const id = 'REGISTER';
@@ -11,6 +14,28 @@ class Register extends StatefulWidget {
 class RegisterState extends State<Register> {
   String email = '';
   String password = '';
+
+  final String API_URL = 'http://localhost:5000/';
+  Map<String, dynamic> pong = {};
+
+  Future<void> pingAPI() async {
+    try {
+      var res = await http
+          .get(Uri.parse(API_URL), headers: {"Accept": "application/json"});
+      var resBody = json.decode(res.body);
+      setState(() {
+        pong = resBody;
+      });
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.pingAPI();
+  }
 
   Future<void> registerUser() async {
     print(email);
@@ -26,22 +51,30 @@ class RegisterState extends State<Register> {
         title: Text('Register'),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: Hero(
-                tag: 'logo',
-                child: CircleAvatar(
-                  backgroundImage: AssetImage(
-                    'assets/images/elixir.jpeg',
-                  ),
-                  radius: 70.0,
-                )),
-          ),
-          SizedBox(height: 18.0),
-          TextField(),
-          TextField(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Hero(
+                  tag: 'logo',
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage(
+                      'assets/images/elixir.jpeg',
+                    ),
+                    radius: 70.0,
+                  )),
+            ),
+            SizedBox(height: 18.0),
+            TextField(
+              onChanged: (val) => email = val,
+            ),
+            TextField(
+              onChanged: (val) => password = val,
+            ),
+            Text(pong['success'] == null ? 'Loading' : pong['message'])
+          ],
+        ),
       ),
     );
   }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,18 @@
-import fastify from 'fastify';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 import 'colors';
 
 const main = async () => {
-  const app = fastify();
-  app.register(require('./routes'));
+  const app = express();
+
+  app.use(cors());
+
+  app.use(morgan('dev'));
+
+  app.get('/', (_, res) => {
+    res.status(200).json({ success: true, message: 'API up and running' });
+  });
 
   const PORT = process.env.PORT || 5000;
   app.listen(PORT, () => {
